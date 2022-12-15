e1bfab873bb2068f1a0e1312713897f98437eee3
IgorStrangarevic <igor.strangarevic@outlook.com>
2022-12-15 09:20:41 +0100

    created MergedArrays class

--- src/MergedArrays.java
+++ src/MergedArrays.java
@@ -2,25 +2,6 @@ import java.util.Arrays;
 
 public class MergedArrays {
 
-        public static void main(String[] args) {
 
-            int [] arr1 = {1,2,3,4};
-            int [] arr2 = {5,6,7,8};
-
-            int [] arr3 = new int [arr1.length+ arr2.length];
-            int j=0;
-            for (int each : arr1) {
-                arr3[j]=each;
-                j++;
-            }
-
-            for (int each : arr2) {
-                arr3[j]=each;
-                j++;
-
-            }
-            System.out.println("arr3 = " + Arrays.toString(arr3));
-
-        }
     }
 
